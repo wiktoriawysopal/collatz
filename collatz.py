beb745b954d5c7afc8683560a49df1d5845609b5
Wiktoria Wysopal <00376472@gmit.ie>
2019-09-28 22:01:05 +0100

    Comments added

--- collatz.py
+++ collatz.py
@@ -1,12 +1,17 @@
+# The bumber we will perform the collatz operation
 n = int(input("Enter a positive integer: "))
 
+# Keep looping until we reach 1.
+# Note: this assuemes the Collatz conjecture is true
 while n != 1:
+    # Print the current value of n 
     print(n)
-
+    # Check if n is even
     if n % 2 == 0:
-
+        # If n is even, divide it by two.
         n = n // 2
     else:
+        # If n is odd, multiplay by three and add 1.
         n = (3 * n) + 1
 
 print(n)
